162d6b6160d6f5bd5763851ec9fd4f1e3413c87b
ETHAN MC DONNELL <G00355252@gmit.ie>
2019-01-23 15:51:52 +0000

    Lab1 changes

---
+++ test2.js
@@ -0,0 +1,12 @@
+var isDone = false;
+var decimal = 6;
+var hex = 0xf00d;
+var binary = 10;
+var octal = 484;
+var color = "blue";
+color = 'red';
+var fullName = "Bob Bobbington";
+var age = 37;
+var sentence = "Hello, my name is " + fullName + ".\n\n    I'll be " + (age + 1) + " years old next month.";
+var sentence1 = "Hello, my name is " + fullName + ".\n\n" +
+    "I'll be " + (age + 1) + " years old next month.";
